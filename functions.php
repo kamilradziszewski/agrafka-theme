9f49b769aecbeb4213c7d2e5427e9758c4eaa6b7
Kamil Radziszewski <contact@kamilradziszewski.com>
2018-02-05 13:28:06 +0100

    Add required plugins script

--- functions.php
+++ functions.php
@@ -62,7 +62,8 @@ class StarterSite extends TimberSite {
 new StarterSite();
 
 
-/**
+
+/***************************************************************************
  * Enqueue scripts and styles
  */
 function agrafka_scripts() {
@@ -80,7 +81,10 @@ function agrafka_scripts() {
 add_action( 'wp_enqueue_scripts', 'agrafka_scripts' );
 
 
-/**
+
+
+
+/***************************************************************************
  * Add class in nav menu when viewing single post
  */
 function my_special_nav_class( $classes, $item ) {
@@ -92,3 +96,67 @@ function my_special_nav_class( $classes, $item ) {
 }
 
 add_filter( 'nav_menu_css_class', 'my_special_nav_class', 10, 2 );
+
+
+
+
+
+/***************************************************************************
+ * Include the TGM_Plugin_Activation class.
+ */
+require_once get_template_directory() . '/class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'agrafka_register_required_plugins' );
+
+function agrafka_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+
+		array(
+			'name'      => 'Timber',
+			'slug'      => 'timber-library',
+			'required'  => true,
+			'force_activation'   => true,
+		),
+
+		array(
+			'name'      => 'Rich Text Excerpts',
+			'slug'      => 'rich-text-excerpts',
+			'required'  => true,
+			'force_activation'   => true,
+		),
+
+		array(
+			'name'      => 'WPFront Scroll Top',
+			'slug'      => 'wpfront-scroll-top',
+			'required'  => true,
+			'force_activation'   => true,
+		),
+
+		array(
+			'name'      => 'All-in-One WP Migration',
+			'slug'      => 'all-in-one-wp-migration',
+			'required'  => false,
+		),
+
+	);
+
+	$config = array(
+		'id'           => 'agrafka',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
